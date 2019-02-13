22d2e856dce03a4b00b0134152fb47d64b31bb44
Cillié Malan <cilliemalan@gmail.com>
2019-02-13 09:48:23 +0200

    Added Unit Test Projects

---
+++ ObservableWebsockets.Tests.Owin/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ObservableWebsockets.Tests.Owin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ObservableWebsockets.Tests.Owin")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00538c01-85bb-41f2-a051-548eb3c7f219")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
